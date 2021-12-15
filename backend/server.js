2671fcaec0ff6b3257871a24b6ac6158244ac261
Fabian Hohn <61757770+CptCringe@users.noreply.github.com>
2021-12-15 15:30:40 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,6 @@ require('./routes/user.routes.js')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8081;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server is running on port ${PORT}.`);
 });
