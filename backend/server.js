eb82edd14061e2f5aa1031f9750d1ab9d0d6108f
Fabian Hohn <61757770+CptCringe@users.noreply.github.com>
2021-12-15 14:57:06 +0100

    Bissi aufgeräumt

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ require('./routes/auth.routes.js')(app);
 require('./routes/user.routes.js')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
