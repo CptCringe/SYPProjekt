50d226c50e1e08e74c82492c6591e9099fe8cacc
ZnarfOreuga <150560@studierende.htl-donaustadt.at>
2022-02-23 15:07:34 +0100

    chat

--- simplechat/server.js
+++ simplechat/server.js
@@ -1,5 +1,19 @@
-const io = require('socket.io')(3000)
+const io = require('socket.io')(63342)
+
+//wir speichern die users als Objekte
+const users = {}
 
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World')
+    socket.on('new-user', name =>{
+        users[socket.id] = name //Der ID = name der User
+        socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message =>{
+        //Benutzer die mit dem Server verbunden sind, bekommen die gleiche Nachricht außer der Sender selbst.
+        socket.broadcast.emit('chat-message', {message :message, name : users[socket.id]})
+    })
+    socket.on('disconnect', () =>{
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
 })
